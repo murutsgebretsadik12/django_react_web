24a3ca3010006f26c12921df3fc00bd905f98bd4
Muruts Gebretsadik <murutsmenge21@Murutss-MacBook-Air.local>
2024-07-13 16:40:55 +0100

    update settings.py

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -107,15 +107,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"]= dj_database_url.parse(database_url)
+# database_url = os.environ.get("DATABASE_URL")
+# DATABASES["default"]= dj_database_url.parse(database_url)
 
 
 # Password validation
